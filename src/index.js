55a04b5ee1840180ccf824eb566ef541d5982087
Jack <Jackson.contato24@hotmail.com>
2022-01-25 16:15:49 -0300

    Type of params

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, res) => {
+  // Query Params
+  const query = req.query;
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  // Body Params
+  const body = req.body;
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  // Route Params
+  const { id } = req.params;
   return res.json(["Curso 8", "Curso 2", "Curso 3"]);
 });
 
