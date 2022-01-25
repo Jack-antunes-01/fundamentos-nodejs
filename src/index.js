283022b4a4e08f7959b2db7b21ed90431019f7ad
Jack <Jackson.contato24@hotmail.com>
2022-01-25 15:51:29 -0300

    Using http methods

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world - Fundamentos" });
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Curso 8", "Curso 2", "Curso 3"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.listen(3000);
