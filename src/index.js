4cd12c1248f37693bd1f7cde9fa142d99e573e1b
Jack <Jackson.contato24@hotmail.com>
2022-01-25 16:34:23 -0300

    Account created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world - Fundamentos" });
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * id - uuid
+ * cpf - string
+ * name - string
+ * statement []
+ */
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({ cpf, name, id, statement: [] });
+
+  return res.send(customers);
 });
 
 app.listen(3000);
